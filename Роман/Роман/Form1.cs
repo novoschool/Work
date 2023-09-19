cb4819331565c87d7a81f87fb892452b5e89167a
DESKTOP-0TBB9S2\Пользователь <t@t.ru>
2023-09-19 10:18:34 +0700

    Проэкт создан по моему вооброжению.

---
+++ Роман/Роман/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Роман
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            var mainPen = new Pen(Color.Black, 3);
+            g.FillRectangle(Brushes.DarkGray, 100, 100, 200, 300);
+            g.DrawRectangle(mainPen, 100, 100, 200, 300);
+        }
+    }
+}
