0fb0cc85f23021196928dc7cbb1166c038f1ea8f
DESKTOP-1A03KKA\PC <t@t.ru>
2023-09-19 11:59:22 +0700

    Звезда есть

--- Angola/Angola/Form1.cs
+++ Angola/Angola/Form1.cs
@@ -26,7 +26,6 @@ namespace Angola
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             var g = CreateGraphics();
-            var Black = new Pen(Color.Black);
             var clientRectangle = ClientRectangle;
             var Hight = clientRectangle.Height;
             var Width = clientRectangle.Width;
@@ -34,7 +33,7 @@ namespace Angola
             g.FillRectangle(new SolidBrush(Color.Red), 0, 0, Width, Height/2);
             g.FillRectangle(new SolidBrush(Color.Black), 0, Hight/2, Width, Height / 2);
 
-            Star((Width/12) * 5, (Height/14) * 5, Width/10, Hight/7);
+            Star((Width/12) * 5, ((Hight/18) * 6), Width/10, Hight/7);
 
 
             
@@ -42,13 +41,25 @@ namespace Angola
 
         void Star(int X, int Y, int Width, int Hight)
         {
-
+            var g = CreateGraphics();
 
             Point[] points1 = new Point[3];
             points1[0] = new Point(X + (Width/2), Y);
             points1[1] = new Point(X + (Width/6), Y + Hight);
             points1[2] = new Point(X + ((Width/6)*4), Y + ((Hight/10)*6));
-            g.FillPolygon(new SolidBrush(Color.Black), points1);
+            g.FillPolygon(new SolidBrush(Color.Yellow), points1);
+
+            Point[] points2 = new Point[3];
+            points2[0] = new Point(X + (Width / 2), Y);
+            points2[1] = new Point(X + (Width / 6)*5, Y + Hight);
+            points2[2] = new Point(X + (Width / 6)*2, Y + ((Hight / 10) * 6));
+            g.FillPolygon(new SolidBrush(Color.Yellow), points2);
+
+            Point[] points3 = new Point[3];
+            points3[0] = new Point(X, Y + (Hight/10)*4);
+            points3[1] = new Point(X + Width, Y + (Hight/10) * 4);
+            points3[2] = new Point(X + Width/2, Y + ((Hight/7) * 5));
+            g.FillPolygon(new SolidBrush(Color.Yellow), points3);
         }
     }
 }
