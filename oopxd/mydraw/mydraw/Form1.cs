067412204b1740bede65ece54f921d83f4274e93
DESKTOP-GHVBEU0\default_user <t@t.ru>
2023-09-22 12:05:54 +0700

    //

--- oopxd/mydraw/mydraw/Form1.cs
+++ oopxd/mydraw/mydraw/Form1.cs
@@ -12,9 +12,12 @@ namespace mydraw
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        private void Form1(PaintEventArgs e) {
+            Pen blackpen = new Pen(Brushes.Black);
+            blackpen.Width = 8.0F;
+            blackpen.LineJoin = System.Drawing.Drawing2D.LineJoin.Bevel;
+            e.Graphics.DrawRectangle(blackpen,
+                new Rectangle(40, 40, 150, 200));
+                }
     }
 }
