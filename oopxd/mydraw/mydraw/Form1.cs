62abf080dd63b26ffd2f739adc7eb29a40a3a971
DESKTOP-GHVBEU0\default_user <t@t.ru>
2023-09-22 12:07:47 +0700

    ///

--- oopxd/mydraw/mydraw/Form1.cs
+++ oopxd/mydraw/mydraw/Form1.cs
@@ -12,7 +12,7 @@ namespace mydraw
 {
     public partial class Form1 : Form
     {
-        private void Form1(PaintEventArgs e) {
+        private void ShowLineJoin(PaintEventArgs e) {
             Pen blackpen = new Pen(Brushes.Black);
             blackpen.Width = 8.0F;
             blackpen.LineJoin = System.Drawing.Drawing2D.LineJoin.Bevel;
