480a603eae658e46473280107cb329908d19f5e5
DESKTOP-L3A7A26\pc <t@t.ru>
2023-09-26 12:04:47 +0700

    3 из 5 звезд готовы !))))

--- Templates/Project 1. Graphics/PictureApp2/PictureApp/Painter.cs
+++ Templates/Project 1. Graphics/PictureApp2/PictureApp/Painter.cs
@@ -33,7 +33,7 @@ namespace PictureApp
                 new PointModel { X = 4.1, Y = 7},
                 new PointModel { X = 3.5, Y = 6.45},
 
-                new PointModel { X = 3.9, Y = 5.6},
+                new PointModel { X = 3.9, Y = 5.6}, //2
                 new PointModel { X = 3, Y = 6.1},
 
                 new PointModel { X = 2.1, Y = 5.6},
@@ -54,13 +54,53 @@ namespace PictureApp
              */
             var pointsR = new[]
             {
-                new PointModel { X = 4.5, Y = 7.8 },
-              
+                new PointModel { X = 5.5, Y = 6.1 },
+                new PointModel { X = 5.4, Y = 5.8 },
+                new PointModel { X = 5.6, Y = 5.8 },
 
+                new PointModel { X = 5.8, Y = 5.8 },
+                new PointModel { X = 5.65, Y = 5.6 },
+
+                new PointModel { X = 5.8, Y = 5.35 },
+                new PointModel { X = 5.5, Y = 5.5 },
+
+                new PointModel { X = 5.2, Y = 5.35 },
+                new PointModel { X = 5.35, Y = 5.6 },
+
+                new PointModel { X = 5.2, Y = 5.8 },
+                new PointModel { X = 5.6, Y = 5.8 },
             };
+
+
             var calculatedPointsR = pointsR.Select(p => scaler.Calculate(p)).ToArray();
             g.FillPolygon(Brushes.Yellow, calculatedPointsR);
             g.DrawPolygon(mainPen, calculatedPointsR);
+
+            var pointsE = new[]
+            {
+                new PointModel { X = 5.35, Y = 7 },
+                new PointModel { X = 5.55, Y =  6.9 },
+                new PointModel { X = 5.55, Y = 7.06},
+
+                new PointModel { X = 5.55, Y = 7.27 },
+                new PointModel { X = 5.7, Y = 7.12 },
+
+                new PointModel { X = 5.88, Y = 7.18 },
+                new PointModel { X = 5.78, Y = 7 },
+
+                new PointModel { X = 5.9, Y = 6.84 },
+                new PointModel { X = 5.7, Y = 6.9 },
+
+
+                new PointModel { X = 5.6, Y = 6.7 },
+                new PointModel { X = 5.55, Y = 7.06 },
+
+            };
+            var calculatedPointsE = pointsE.Select(p => scaler.Calculate(p)).ToArray();
+            g.FillPolygon(Brushes.Yellow, calculatedPointsE);
+            g.DrawPolygon(mainPen, calculatedPointsE);
+
+
         }
     }
 }
