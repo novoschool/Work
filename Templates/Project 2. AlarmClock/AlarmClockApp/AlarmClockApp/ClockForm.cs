edcdda0c1228909df20bbd2987fb58ad52e5cea6
Тимкин Владимир <vtimkin@unitarius.ru>
2023-09-28 23:27:38 +0700

    Project 3 created

---
+++ Templates/Project 2. AlarmClock/AlarmClockApp/AlarmClockApp/ClockForm.cs
@@ -0,0 +1,38 @@
+﻿using AlarmClockApp.Forms;
+using AlarmClockApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmClockApp
+{
+    public partial class ClockForm : Form
+    {
+        ClockModel _model = new ClockModel();
+
+        public ClockForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SettingsButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = _model.CurrentTime.ToString(@"hh\:mm\:ss");
+        }
+
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+            new AboutForm().ShowDialog();
+        }
+    }
+}
---
+++ Templates/Project 2. AlarmClock/AlarmClockApp/AlarmClockApp/Models/ClockModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClockApp.Models
+{
+    internal class ClockModel
+    {
+        public TimeSpan CurrentTime => DateTime.Now.TimeOfDay;
+    }
+}
