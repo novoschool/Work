5b1b625fd5a1b8332c74d6de92f876e079a360cb
DESKTOP-3TGE7Q5\Пользователь <hxfjbjk12@gmail.com>
2023-09-29 11:57:19 +0700

    АК 47 начало

--- egorbylina/egorbylina/egorbylina/Form1.cs
+++ egorbylina/egorbylina/egorbylina/Form1.cs
@@ -1,3 +1,5 @@
+using PictureApp;
+
 namespace egorbylina
 {
     public partial class Form1 : Form
@@ -5,6 +7,17 @@ namespace egorbylina
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var painter = new Painter();
+            painter.Paint(e.Graphics, ClientRectangle);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Refresh();
         }
     }
 }
