93e520705c1f52227156e0eded2fa7672e7b9498
DESKTOP-3TGE7Q5\Пользователь <hxfjbjk12@gmail.com>
2023-10-03 10:37:13 +0700

    Домик

---
+++ aegis3/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aegis3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.SizeChanged += new EventHandler(Form1_SizeChanged);
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            var mainPen = new Pen(Color.Black, 1);
+            var mainPen3 = new Pen(Color.Black, 2);
+            var mainPen2 = new Pen(Color.SaddleBrown, 10);
+            var mainPen4 = new Pen(Color.White, 3);
+
+            float scaleFactor = Math.Min(Width / 1000.0f, Height / 1000.0f);
+            g.ScaleTransform(scaleFactor, scaleFactor);
+
+            g.FillRectangle(Brushes.Green, 50, 650, 900, 300);
+            g.DrawRectangle(mainPen, 50, 650, 900, 300);
+
+            g.DrawEllipse(mainPen2, 400, 350, 150, 150);
+            g.FillEllipse(Brushes.Yellow, 50, 50, 150, 150);
+
+            g.FillRectangle(Brushes.Chocolate, 200, 300, 600, 600);
+            g.DrawRectangle(mainPen, 200, 300, 600, 600);
+
+            g.FillRectangle(Brushes.SaddleBrown, 425, 600, 150, 300);
+            g.DrawRectangle(mainPen, 425, 600, 150, 300);
+
+            g.DrawEllipse(mainPen2, 400, 350, 200, 200);
+            g.FillEllipse(Brushes.Aqua, 400, 350, 200, 200);
+
+            g.DrawLine(mainPen4, 400, 450, 600, 450);
+            g.DrawLine(mainPen4, 500, 350, 500, 550);
+
+            g.DrawEllipse(mainPen3, 550, 725, 15, 15);
+            g.FillEllipse(Brushes.SaddleBrown, 550, 725, 15, 15);
+
+            g.FillRectangle(Brushes.GreenYellow, 870, 800, 10, 40);
+            g.DrawRectangle(mainPen, 870, 800, 10, 40);
+
+            g.FillEllipse(Brushes.Blue, 845, 785, 20, 20);
+            g.FillEllipse(Brushes.Blue, 885, 785, 20, 20);
+            g.FillEllipse(Brushes.Blue, 865, 765, 20, 20);
+            g.FillEllipse(Brushes.Blue, 880, 770, 20, 20);
+            g.FillEllipse(Brushes.Blue, 850, 770, 20, 20);
+            g.FillEllipse(Brushes.Blue, 865, 800, 20, 20);
+            g.FillEllipse(Brushes.Blue, 880, 795, 20, 20);
+            g.FillEllipse(Brushes.Blue, 850, 795, 20, 20);
+
+            g.DrawEllipse(mainPen3, 860, 780, 30, 30);
+            g.FillEllipse(Brushes.Red, 860, 780, 30, 30);
+
+            Point point1 = new Point(100, 300);
+            Point point2 = new Point(200, 100);
+            Point point3 = new Point(300, 300);
+
+            int xShift = 50;
+
+            point1.X = (int)((point1.X - xShift) * 3 + xShift);
+            point2.X = (int)((point2.X - xShift) * 3 + xShift);
+            point3.X = (int)((point3.X - xShift) * 3 + xShift);
+
+            Point[] trianglePoints = { point1, point2, point3 };
+            g.FillPolygon(Brushes.SaddleBrown, trianglePoints);
+
+
+        }
+    }
+}
---
+++ aegis3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("aegis3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sborka")]
+[assembly: AssemblyProduct("aegis3")]
+[assembly: AssemblyCopyright("Copyright © sborka 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("3bf3df0c-09a9-48e8-a39b-e946d15dd8ef")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
