cf3f61a2f38ced381ce1b1c36db073ee6a3132ac
DESKTOP-0TBB9S2\Пользователь <t@t.ru>
2023-10-17 09:16:34 +0700

    .

--- Роман/AlarmClock.0.1/AlarmClock.0.1/Forms1/SettingsForm.Designer.cs
+++ Роман/AlarmClock.0.1/AlarmClock.0.1/Forms1/SettingsForm.Designer.cs
@@ -82,6 +82,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "апа";
             // 
             // textBox2
             // 
