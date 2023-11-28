452874ce2127f1f9ae1b928750627fd8a6ae4f3c
vtimkin <vtimkin@unitarius.ru>
2023-11-28 19:58:15 +0700

    Added WpfTest project

---
+++ Templates/Project 4. Calculator/Calculator2/Calculator2.Business/Models/CalculatorModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2.Business.Models
+{
+    public class CalculatorModel
+    {
+        public double XRegister { get; set; }
+    }
+}
---
+++ Templates/Project 4. Calculator/Calculator2/Calculator2.Business/Services/CalculatorService.cs
@@ -0,0 +1,22 @@
+﻿using Calculator2.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2.Business.Services
+{
+    public class CalculatorService
+    {
+        public void AddDigit(CalculatorModel model, int digit)
+        {
+            model.XRegister = model.XRegister * 10 + digit;
+        }
+
+        public void Clear(CalculatorModel model)
+        {
+            model.XRegister = 0;
+        }
+    }
+}
