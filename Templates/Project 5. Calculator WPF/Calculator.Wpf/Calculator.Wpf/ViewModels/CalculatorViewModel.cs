2f7d649086951417bb12f043181b75c84928e39e
vtimkin <vtimkin@unitarius.ru>
2023-12-12 22:51:41 +0700

    Calculator Wpf created

---
+++ Templates/Project 5. Calculator WPF/Calculator.Wpf/Calculator.Wpf/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Calculator2.Business.Models;
+using Calculator2.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Wpf.ViewModels
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private CalculatorService _service = new CalculatorService();
+
+        private CalculatorModel _model = new CalculatorModel();
+
+        public string DisplayString => _model.XRegister.ToString();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void PressDigit(string digitStr)
+        {
+            if (int.TryParse(digitStr, out var digit))
+            {
+                _service.AddDigit(_model, digit);
+                OnPropertyChanged(nameof(DisplayString));
+            }
+        }
+
+        public void Clear()
+        {
+            _service.Clear(_model);
+            OnPropertyChanged(nameof(DisplayString));
+        }
+
+        public void PressOperator(string operatorStr)
+        {
+            _service.InsertOperator(_model, operatorStr);
+            OnPropertyChanged(nameof(DisplayString));
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
