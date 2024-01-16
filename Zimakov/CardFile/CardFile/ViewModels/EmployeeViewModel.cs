efb682ba9a207d1239eabc9e15f33f823609c7ca
DESKTOP-3TGE7Q5\Пользователь <hxfjbjk12@gmail.com>
2024-01-16 09:53:01 +0700

    pupupu

---
+++ Zimakov/CardFile/CardFile/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ManufacturedProduction { get; set; }
+
+        public decimal ValueOutputProduction { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime CreationDate { get; set; } = new DateTime(2000, 1, 1);
+
+        public string BirthDateText => CreationDate.ToShortDateString();
+
+        public int RenovationNeed { get; set; }
+
+        public int EmployeesNumber { get; set; }
+
+        public DateTime LastRenovationDate { get; set; } = new DateTime(2020, 1, 1);
+
+        public string EmploymentDateText => LastRenovationDate.ToShortDateString();
+    }
+}
