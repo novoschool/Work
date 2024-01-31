7fe5b17d8cb3f4d61f1692370994a065aabd03f3
vtimkin <vtimkin@unitarius.ru>
2024-02-01 00:22:08 +0700

    Updated hash table

--- Algorithmization/HashTable/HashTable/Entities/MyHashTable.cs
+++ Algorithmization/HashTable/HashTable/Entities/MyHashTable.cs
@@ -12,25 +12,46 @@ namespace HashTable.Entities
 
         private const int MaxFilledPercent = 70;
 
-        private KeyValuePair[] _dataSet = new KeyValuePair[InitialArraySize];
+        private KeyValuePair?[] _dataSet = new KeyValuePair[InitialArraySize];
 
         public int Count { get; private set; }
 
+        private int _busyCount = 0;
+
         public TValue? Get(TKey key)
         {
+            var hash = (uint)key.GetHashCode();
+            var index = hash % _dataSet.LongLength;
+
+            while (_dataSet[index] != null)
+            {
+                if (_dataSet[index].Key.Equals(key))
+                {
+                    if (_dataSet[index].IsRemoved)
+                    {
+                        return default;
+                    }
+
+                    return _dataSet[index].Value;
+                }
+
+                index = GetNextIndex(key, index, _dataSet.Length);
+            }
+
             return default;
         }
 
         public bool Add(TKey key, TValue value)
         {
-            var hash = key.GetHashCode();
-            var index = hash % _dataSet.Length;
+            var hash = (uint)key.GetHashCode();
+            var index = hash % _dataSet.LongLength;
 
             while (_dataSet[index] != null)
             {
                 if (_dataSet[index].Key.Equals(key))
                 {
                     _dataSet[index].Value = value;
+                    _dataSet[index].IsRemoved = false;
                     return false;
                 }
 
@@ -39,16 +60,44 @@ namespace HashTable.Entities
 
             _dataSet[index] = new KeyValuePair { Key = key, Value = value };
             Count++;
+            _busyCount++;
             CheckAndRefill();
             return true;
         }
 
-        private int GetNextIndex(TKey key, int index, int count)
+        public bool Remove(TKey key)
         {
-            index++;
-            if (index == count)
+            var hash = (uint)key.GetHashCode();
+            var index = hash % _dataSet.LongLength;
+
+            while (_dataSet[index] != null)
             {
-                index = 0;
+                if (_dataSet[index].Key.Equals(key))
+                {
+                    if (_dataSet[index].IsRemoved)
+                    {
+                        return false;
+                    }
+
+                    _dataSet[index].IsRemoved = true;
+                    Count--;
+                    return true;
+                }
+
+                index = GetNextIndex(key, index, _dataSet.Length);
+            }
+
+            return false;
+        }
+
+        private long GetNextIndex(TKey key, long index, long count)
+        {
+            var hashCode = (uint)key.GetHashCode();
+            var indexStep = hashCode % (_dataSet.LongLength - 1) + 1;
+            index += indexStep;
+            if (index >= count)
+            {
+                index -= count;
             }
 
             return index;
@@ -56,21 +105,26 @@ namespace HashTable.Entities
 
         private void CheckAndRefill()
         {
-            if (_dataSet.Length * MaxFilledPercent < Count * 100)
+            if (_dataSet.Length * MaxFilledPercent > _busyCount * 100)
             {
                 return;
             }
 
-            var newArraySize = _dataSet.Length * 2 + 1;
+            var newArraySize = _dataSet.LongLength * 2 + 1;
+            while (!IsPrime(newArraySize))
+            {
+                newArraySize += 2;
+            }
+
             var newArray = new KeyValuePair[newArraySize];
             foreach (var kvp in _dataSet)
             {
-                if (kvp == null)
+                if (kvp == null || kvp.IsRemoved)
                 {
                     continue;
                 }
 
-                var hash = kvp.Key.GetHashCode();
+                var hash = (uint)kvp.Key.GetHashCode();
                 var index = hash % newArraySize;
 
                 while (newArray[index] != null)
@@ -90,13 +144,37 @@ namespace HashTable.Entities
             }
 
             _dataSet = newArray;
+            _busyCount = Count;
+        }
+
+        private bool IsPrime(long number)
+        {
+            if (number % 2 == 0)
+            {
+                return false;
+            }
+
+            var divider = 3;
+            while (divider * divider <= number)
+            {
+                if (number % divider == 0)
+                {
+                    return false;
+                }
+
+                divider += 2;
+            }
+
+            return true;
         }
 
         internal class KeyValuePair
         {
             public TKey Key { get; set; }
 
-            public TValue Value { get; set; }
+            public TValue? Value { get; set; }
+
+            public bool IsRemoved { get; set; }
         }
     }
 }
