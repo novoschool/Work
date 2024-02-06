9dde0e7993490c166aed54b92ee0aabfd7776d20
DESKTOP-529RDNR\Admin <t@t.ru>
2024-02-06 11:54:35 +0700

    PostgreSQL added

---
+++ VT/Project 6. CardFile/CardFile/CardFile.Business/Services/EmployeeService.cs
@@ -0,0 +1,42 @@
+﻿using CardFile.Core.Entities;
+using CardFile.Core.Repositories;
+using CardFile.DataAccess.PostreSQL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.Business.Services
+{
+    public class EmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeService() 
+        {
+            _employeeRepository = new EmployeeRepository();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeRepository.GetAll();
+        }
+
+        public void SaveEmployee(Employee employee)
+        {
+            _employeeRepository.Save(employee);
+        }
+
+        public void Delete(int employeeId)
+        {
+            //var existingEmployee = _storage.Employees.FirstOrDefault(e => e.Id == employeeId);
+            //if (existingEmployee == null)
+            //{
+            //    throw new IndexOutOfRangeException();
+            //}
+
+            //_storage.Employees.Remove(existingEmployee);
+        }
+    }
+}
---
+++ VT/Project 6. CardFile/CardFile/CardFile.Core/Entities/EmployeeCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CardFile.Core.Entities
+{
+    [Serializable]
+    [XmlRoot("EmployeeCollection")]
+    public class EmployeeCollection
+    {
+        [XmlArray("Employees")]
+        [XmlArrayItem("Employee")]
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
