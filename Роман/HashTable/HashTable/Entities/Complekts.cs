6c848ea7439c3097e8707a8250fa601fc4819b1c
vmisha <misha.cerez@gmail.com>
2024-02-13 09:17:56 +0700

    .

---
+++ Роман/HashTable/HashTable/Entities/Complekts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable.Entities
+{
+    internal class Complekts : IEquatable<Complekts>
+    {
+        public string StringProperty { get; set; }
+
+        public int IntProperty { get; set; }
+
+        public bool Equals(Complekts? other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (StringProperty != other.StringProperty)
+            {
+                return false;
+            }
+
+            return IntProperty == other.IntProperty;
+        }
+    }
+}
---
+++ Роман/HashTable/HashTable/Entities/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable.Entities
+{
+    internal class MyDictionary<Tkey, TValue> where Tkey : IEquatable<Tkey>
+    {
+        private readonly MyDictionary<Tkey, TValue> _dictionry = new();
+
+        public TValue? GetValue(Tkey key)
+        {
+            return _dictionry.GetValue(key);
+        }
+
+        public bool Add(Tkey key, TValue value)
+        {
+            return _dictionry.Add(key, value);
+        }
+
+        public bool Remove(Tkey key)
+        {
+            return _dictionry.Remove(key);
+        }
+
+        public bool Count => _dictionry.Count;
+
+        public TValue? this[Tkey key]
+        {
+            get
+            {
+                return _dictionry.GetValue(key);
+            }
+            set
+            {
+                if (value == null)
+                {
+                    _dictionry.Remove(key);
+                    return;
+                }
+
+                _dictionry.Add(key, value);
+            }
+        }
+    }
+}
