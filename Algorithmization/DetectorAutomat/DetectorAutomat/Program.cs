ece38c69c2fb17e02139bd6e932ea6107293cbd0
DESKTOP-3TGE7Q5\Пользователь <hxfjbjk12@gmail.com>
2024-03-07 10:22:13 +0700

    .

---
+++ Algorithmization/DetectorAutomat/DetectorAutomat/Program.cs
@@ -0,0 +1,31 @@
+﻿using DetectorAutomat.Services;
+
+namespace DetectorAutomat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите строку: ");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                var detector = new StringDetector();
+                if (detector.Detect(input))
+                {
+                    Console.WriteLine("Строка допущена.");
+                }
+                else
+                {
+                    Console.WriteLine("Строка отвергнута.");
+                }
+            }
+        }
+    }
+}
---
+++ Algorithmization/DetectorAutomat/DetectorAutomat/Services/StringDetector.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetectorAutomat.Services
+{
+    public class StringDetector
+    {
+        private State _state;
+
+        public bool Detect(string input)
+        {
+            _state = State.Start;
+            foreach (var ch in input)
+            {
+                if (char.IsWhiteSpace(ch))
+                {
+                    continue;
+                }
+
+                var charClass = Classify(ch);
+                ReadChar(charClass);
+            }
+
+            ReadChar(CharClass.EndMarker);
+
+            return _state == State.Success;
+        }
+
+        private void ReadChar(CharClass charClass)
+        {
+            switch (_state)
+            {
+                case State.None:
+                    _state = State.Error;
+                    break;
+                case State.Start:
+                    switch (charClass)
+                    {
+                        case CharClass.Alpha:
+                            _state = State.Letter;
+                            break;
+                        case CharClass.EndMarker:
+                            _state = State.Fail;
+                            break;
+                        default:
+                            _state = State.Error;
+                            break;
+                    }
+                    break;
+                case State.Letter:
+                    switch (charClass)
+                    {
+                        case CharClass.LeftBracket:
+                            _state = State.LeftBracket;
+                            break;
+                        case CharClass.Comma:
+                            _state = State.VariableSeparator;
+                            break;
+                        case CharClass.EndMarker:
+                            _state = State.Success;
+                            break;
+                        default:
+                            _state = State.Error;
+                            break;
+                    }
+                    break;
+                case State.VariableSeparator:
+                    switch (charClass)
+                    {
+                        case CharClass.Alpha:
+                            _state = State.Letter;
+                            break;
+                        case CharClass.EndMarker:
+                            _state = State.Fail;
+                            break;
+                        default:
+                            _state = State.Error;
+                            break;
+                    }
+                    break;
+                case State.LeftBracket:
+                    switch (charClass)
+                    {
+                        case CharClass.Digit:
+                            _state = State.Dimension;
+                            break;
+                        case CharClass.EndMarker:
+                            _state = State.Fail;
+                            break;
+                        default:
+                            _state = State.Error;
+                            break;
+                    }
+                    break;
+                case State.Dimension:
+                    switch (charClass)
+                    {
+                        case CharClass.RightBracket:
+                            _state = State.RightBracket;
+                            break;
+                        case CharClass.Comma:
+                            _state = State.DimensionSeparator;
+                            break;
+                        case CharClass.EndMarker:
+                            _state = State.Fail;
+                            break;
+                        default:
+                            _state = State.Error;
+                            break;
+                    }
+                    break;
+                case State.RightBracket:
+                    switch (charClass)
+                    {
+                        case CharClass.Comma:
+                            _state = State.VariableSeparator;
+                            break;
+                        case CharClass.EndMarker:
+                            _state = State.Success;
+                            break;
+                        default:
+                            _state = State.Error;
+                            break;
+                    }
+                    break;
+                case State.DimensionSeparator:
+                    switch (charClass)
+                    {
+                        case CharClass.Digit:
+                            _state = State.Dimension;
+                            break;
+                        case CharClass.EndMarker:
+                            _state = State.Fail;
+                            break;
+                        default:
+                            _state = State.Error;
+                            break;
+                    }
+                    break;
+                default:
+                    _state = State.Error;
+                    break;
+            }
+        }
+
+        private CharClass Classify(char ch)
+        {
+            //switch (ch)
+            //{
+            //    case ',':
+            //        return CharClass.Comma;
+
+            //    case '(':
+            //        return CharClass.LeftBracket;
+
+            //    case ')':
+            //        return CharClass.RightBracket;
+
+            //    default:
+            //        break;
+            //}
+
+            return 
+                (ch, char.IsLetter(ch), char.IsDigit(ch)) switch
+                {
+                    (',', _, _) => CharClass.Comma,
+                    ('(', _, _) => CharClass.LeftBracket,
+                    (')', _, _) => CharClass.RightBracket,
+                    (_, true, _) => CharClass.Alpha,
+                    (_, false, true) => CharClass.Digit,
+                    _ => CharClass.Other,
+                };
+
+            //if (char.IsLetter(ch))
+            //{
+            //    return CharClass.Alpha;
+            //}
+
+            //if (char.IsDigit(ch))
+            //{
+            //    return CharClass.Digit;
+            //}
+
+            //return CharClass.Other;
+        }
+    }
+}
